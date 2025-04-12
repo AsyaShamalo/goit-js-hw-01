f57fa28ac548a2de3253a1cea1b6a6fad209c732
asya <shamalo.n.75@gmail.com>
2025-04-13 00:04:18 +0300

    err corr

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,10 @@
 function getElementWidth(content, padding, border) {
-    let numContent = Number.parseFloat(content);
-    let numPadding = Number.parseFloat(padding);
-    let numBorder = Number.parseFloat(border);
-    console.log(numContent + numPadding * 2 + numBorder * 2);
+    let content = Number.parseFloat(content);
+    let padding = Number.parseFloat(padding) * 2;
+    let border = Number.parseFloat(border) * 2;
+    return content + padding + border;
     
 }
-getElementWidth("50px", "8px", "4px");
-getElementWidth("60px", "12px", "8.5px");
-getElementWidth("200px", "0px", "0px");  
+console.log (getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));  
